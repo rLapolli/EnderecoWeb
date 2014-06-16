d0cd9777da0560b705742baa38d3f874d7f92efb
rLapolli <roberto.lapolli@gmail.com>
2014-06-16 16:57:45 -0300

    Primeiro Commit

---
+++ EnderecosWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnderecosWeb.MvcApplication" Language="C#" %>
---
+++ EnderecosWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnderecosWeb.Startup))]
+namespace EnderecosWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
