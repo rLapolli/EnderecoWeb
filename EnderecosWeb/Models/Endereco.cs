946183b1911a0702edd4512767bb00c75abff395
rLapolli <roberto.lapolli@gmail.com>
2014-06-16 17:13:58 -0300

    Adcionada classe Endereco

---
+++ EnderecosWeb/Models/Endereco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnderecosWeb.Models
+{
+    public class Endereco
+    {
+        public Guid Id { get; set; }
+        public string NomeRua { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+    }
+}
